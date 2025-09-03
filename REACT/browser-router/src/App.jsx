fa5f6254123bb14ccd39c03b8c1600eca5e32a08
devYuno <annykessy20@gmail.com>
2025-09-03 11:48:57 -0300

    Browser Router

---
+++ REACT/browser-router/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './Components/Home'
+import { Sobre } from './Components/Sobre'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/sobre' element={<Sobre/>}></Route>
+        </Routes>
+      </BrowserRouter>     
+    </>
+  )
+}
+
+export default App
